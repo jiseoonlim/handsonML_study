6b8be34cb68f229553bbc167fab3648d2c84196a
jiseoonlim <74448629+jiseoonlim@users.noreply.github.com>
2021-04-17 14:51:56 +0900

    Update mnist_download.py

--- mnist_download.py
+++ mnist_download.py
@@ -28,3 +28,18 @@ plt.imshow(some_digit_image, cmap = matplotlib.cm.binary,
 plt.axis("off")
 plt.show()
 
+# train set / test set
+X_train, X_test, y_train, y_test = X.to_numpy()[:60000], X.to_numpy()[60000:], y[:60000], y[60000:]
+shuffle_index = np.random.permutation(60000)
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+# Training SGDclassifier / 5와 5아님만 구분하는 binary classifier
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(max_iter=5, random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+print(sgd_clf.predict([some_digit])) #결과값 FALSE
